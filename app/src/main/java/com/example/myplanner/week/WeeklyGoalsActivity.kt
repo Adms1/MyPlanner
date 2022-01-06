47f7cb559b783a52081861163fbc8813c24a1658
Admin <admsbuild@gmail.com>
2022-01-06 10:15:48 +0530

    commit antra 06-01-2022

---
+++ app/src/main/java/com/example/myplanner/week/WeeklyGoalsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myplanner.week
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeeklyGoalsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weekly_goals)
+    }
+}
---
+++ app/src/main/java/com/example/myplanner/week/WeeklyReviewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myplanner.week
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myplanner.R
+
+class WeeklyReviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weekly_review)
+    }
+}
